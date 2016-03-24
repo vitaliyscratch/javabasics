03c4bbcfa8d5c6ccb04bdaad5f5fb85b062546b2
vitaliyscratch <vitalii.yaroshevych@gmail.com>
2016-03-24 13:37:28 +0200

    My first commit with two adds

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int tstA1i = 0;
+        int tstB1i= 0;
+        int tstC1i = 0;
+        int tstD1i = 0;
+
+        double tstA1d = 0;
+        double tstB1d = 0;
+        double tstC1d = 0;
+        double tstD1d = 0;
+
+        char tstA1c = '*';
+        char tstB1c = '*';
+        char tstC1c = '*';
+        char tstD1c = '*';
+        
+        System.out.println("Hello, world!");
+
+
+    }
+}
